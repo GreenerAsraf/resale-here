45c1ad8c98858264b86acdb6c90be8aec0586c38
Asraf407 <Asraf.uddin4407@gmail.com>
2022-11-24 00:44:32 +0600

    react router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Routes/Routes';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className="App">
-      <p className="text-5xl text-primary">Recycle Now</p>
-    </div>
+    <div className='max-w-[1440px] mx-auto'>
+    <RouterProvider router={router}></RouterProvider>
+    <Toaster></Toaster>
+  </div>
   );
 }
 
---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,50 @@
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../../Layout/Main");
+const { default: Home } = require("../../Pages/Home/Home/Home");
+
+
+
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+
+
+
+
+
+
+
+// const router = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <Main></Main>, 
+//         errorElement: <DisplayError></DisplayError>,
+//         children: [
+//             {
+//                 path: '/',
+//                 element: <Home></Home>
+//             },
+//             {
+//                 path: '/login',
+//                 element: <Login></Login>
+//             },
+//             {
+//                 path: '/signup',
+//                 element: <SignUp></SignUp>
+//             },
+//             {
+//                 path: '/appointment',
+//                 element: <Appointment></Appointment>
+//             }
+//         ]
+//     },
